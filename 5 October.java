a6a213d36826d284e0f9918105bb245dab910776
vibhorgupta <vibhor10k@gmail.com>
2023-10-05 16:05:00 -0400

    5 October

---
+++ 5 October.java
@@ -0,0 +1,18 @@
+// Question 1
+public class Solution {
+
+	public static boolean isPalindrome(String str) {
+		//Your code goes here
+		int i =0, j=str.length() -1;
+
+		while(i<j){
+			if(str.charAt(i) != str.charAt(j))
+				return false;
+			
+			i++;
+			j--;
+		}
+		return true;
+	}
+
+}
